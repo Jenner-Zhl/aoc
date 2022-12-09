985999863fbdc8c9b24ef3fd4cd1294684299456
Jenner-Zhl <guduxinlingzl@126.com>
2022-12-09 23:11:12 +0800

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,55 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var max = 0
+        var current = 0
+        input.forEach {
+            if (it != "") {
+                current += it.toInt()
+            } else {
+                max = maxOf(max, current)
+                current = 0
+            }
+            if (current != 0) {
+                max = maxOf(max, current)
+            }
+        }
+        return max
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val top3 = intArrayOf(0, 0, 0)
+        fun submitTop3(n: Int) {
+            var min = Int.MAX_VALUE
+            var minIndex = 0
+            top3.forEachIndexed { index, num ->
+                if (num < min) {
+                    min = num
+                    minIndex = index
+                }
+            }
+            if (n > min) {
+                top3[minIndex] = n
+            }
+        }
+        var current = 0
+        input.forEach {
+            if (it != "") {
+                current += it.toInt()
+            } else {
+                submitTop3(current)
+                current = 0
+            }
+        }
+        if (current != 0) {
+            submitTop3(current)
+        }
+        return top3.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     part1(input).println()
